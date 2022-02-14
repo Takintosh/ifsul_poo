a85c43a9342c0ae106f07811b2310940b8f0987e
Amilcar Santana <takintosh@gmail.com>
2022-02-13 22:11:44 -0300

    Preparatorio Ex1
    Ejercicio previo a evaluación 1

---
+++ preparatorios/bebida.java
@@ -0,0 +1,71 @@
+package preparatorios;
+
+public class bebida {
+
+    private static String nome;
+    private static String descricao;
+    private static float custo, precoVenda;
+    private static int estoque;
+
+    public static void setNome(String nome) {
+        bebida.nome = nome;
+    }
+    public static String getNome() {
+        return bebida.nome;
+    }
+
+    public static void setCusto(float custo) {
+        bebida.custo = custo;
+    }
+    public static float getCusto() {
+        return bebida.custo;
+    }
+
+    public static void setDescricao(String descricao) {
+        bebida.descricao = descricao;
+    }
+    public static String getDescricao() {
+        return bebida.descricao;
+    }
+
+    public static void setEstoque(int estoque) {
+        bebida.estoque = estoque;
+    }
+    public static int getEstoque() {
+        return bebida.estoque;
+    }
+
+    public static float getPrecoVenda() {
+        bebida.precoVenda = (float) (getCusto() * 1.3);
+        return bebida.precoVenda;
+    }
+
+    public static void mostrar() {
+        System.out.print("\nProduto: " + bebida.getNome());
+        System.out.print("\nCusto: " + bebida.getCusto());
+        System.out.print("\nDescriçao: " + bebida.getDescricao());
+        System.out.print("\nEstoque: " + bebida.getEstoque());
+        System.out.print("\nValor de venda do produto: " + bebida.getPrecoVenda());
+    }
+
+    public static String retirar(int quant) {
+        if (bebida.estoque >= quant) {
+            setEstoque(getEstoque()-quant);
+            return ("Estoque atualizado. Estoque atual: " + bebida.estoque);
+        } else {
+            return ("Quantidade maior que o Estoque.");
+        }
+    }
+    public static String adicionar(int quant) {
+        setEstoque(getEstoque()+quant);
+        return ("Estoque atualizado. Estoque atual: " + bebida.estoque);
+    }
+
+    public static void construtor(String nome, Float preco, String descricao, int estoque) {
+        setNome(nome);
+        setCusto(preco);
+        setDescricao(descricao);
+        setEstoque(estoque);
+    }
+
+}
---
+++ preparatorios/ex1.java
@@ -0,0 +1,121 @@
+package preparatorios;
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class ex1 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        String nome, descricao;
+        Float custo;
+        int estoque, quant, opcao = 0;
+
+        System.out.println("------------------------------");
+        while (true) {
+            try {
+                System.out.print("\nIngresse o nome do produto: ");
+                input = new Scanner(System.in);
+                //bebida.setNome(input.nextLine());
+                nome = input.nextLine();
+                break;
+            } catch (InputMismatchException e) {
+                System.out.print("Formato errado, tente novamente.");
+            }
+        }
+
+        while (true) {
+            try {
+                System.out.print("\nIngresse o custo do produto: ");
+                input = new Scanner(System.in);
+                //bebida.setCusto(input.nextFloat());
+                custo = input.nextFloat();
+                break;
+            } catch (InputMismatchException e) {
+                System.out.print("Formato errado, tente novamente.");
+            }
+        }
+        
+        while (true) {
+            try {
+                System.out.print("\nIngresse o descriçao do produto: ");
+                input = new Scanner(System.in);
+                //bebida.setDescricao(input.nextLine());
+                descricao = input.nextLine();
+                break;
+            } catch (InputMismatchException e) {
+                System.out.print("Formato errado, tente novamente.");
+            }
+        }
+        
+        while (true) {
+            try {
+                System.out.print("\nIngresse o estoque do produto: ");
+                input = new Scanner(System.in);
+                //bebida.setEstoque(input.nextInt());
+                estoque = input.nextInt();
+                break;
+            } catch (InputMismatchException e) {
+                System.out.print("Formato errado, tente novamente.");
+            }
+        }
+
+        bebida.construtor(nome, custo, descricao, estoque);
+
+        bebida.mostrar();
+
+        while (true) {
+            try {
+                System.out.println("\nRetirar ou Adicionar no estoque?");
+                System.out.println("1 - Adicionar");
+                System.out.println("2 - Retirar");
+                System.out.println("0 - Fechar");
+                input = new Scanner(System.in);
+                opcao = input.nextInt();
+
+                switch (opcao) {
+
+                    case 1:
+                        while (true) {
+                            try {
+                                System.out.print("\nQuantidade a Adicionar: ");
+                                input = new Scanner(System.in);
+                                quant = input.nextInt();
+                                System.out.println(bebida.adicionar(quant));
+                                break;
+                            } catch (InputMismatchException e) {
+                                System.out.print("Formato errado, tente novamente.");
+                            }
+                        }
+                        break;
+        
+                    case 2:
+                        while (true) {
+                            try {
+                                System.out.print("\nQuantidade a Retirar: ");
+                                input = new Scanner(System.in);
+                                quant = input.nextInt();
+                                System.out.println(bebida.retirar(quant));
+                                break;
+                            } catch (InputMismatchException e) {
+                                System.out.print("Formato errado, tente novamente.");
+                            }
+                        }
+                        break;
+        
+                    case 0:
+                        System.out.print("Finalizando processo...");
+                        System.exit(0);
+                        break;
+        
+                    default:
+                        System.out.print("Formato errado, tente novamente.");
+                        break;
+                }
+                
+            } catch (InputMismatchException e) {
+                System.out.print("Formato errado, tente novamente.");
+            }
+        }
+
+    }
+}
